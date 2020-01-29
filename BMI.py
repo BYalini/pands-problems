6967301f90235057f05cfcd802a6b2f60b6988a0
BYalini <G00387915@gmit.ie>
2020-01-29 22:43:33 +0000

    Week 2 homework assignment

---
+++ BMI.py
@@ -0,0 +1,12 @@
+# This program calculates Body Mass Index (BMI) of a person
+# The user inputs are weight in Kg and height in cm
+
+Weight = float(input ("Enter weight in Kg :"))
+Height = float(input("Enter Height in cm :"))
+
+# The formula for BMI is weight in kilograms divided by height in meters squared
+BMI = Weight / ((Height/100) ** 2)
+
+
+print ("BMI is", round(BMI,2))
+
