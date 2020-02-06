3810a732dbe023dc6933a0810b86d4edd4af3ba4
BYalini <G00387915@gmit.ie>
2020-02-06 10:40:23 +0000

    wk 4 task

--- collatz.py
+++ collatz.py
@@ -1,15 +1,23 @@
 #Weekly task 4
-#Program that asks the user to input any positive integer and outputs the successive values of the following calculation. 
-# At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
-# Have the program end if the current value is one.
+# This program prompts the user for an integer input and prints out the hailstone sequence
+# Collatz conjecture : Regardless of the starting of a postive integer n , the the sequence will always reach 1
+# Proposed by German mathematician Lothar Collatz is 1937. No mathematical proof exists
+# f(n) = n /2 if n is even
+#      = 3n+1 if n is odd
+# The program will stop if the current value is 1 
+#ref:https://en.wikipedia.org/wiki/Collatz_conjecture
+
 
 n = int(input("Please enter a positive integer: "))
+if n <= 1:
+    print("n <= 1, Please try again with a positive integer greater than 1")
+  
 while n > 1: 
-    if n % 2 == 0:
+    print(int(n), end = " ")   
+    if n % 2 == 0: 
         n = n/2
-    else:
+    else: # n is odd and greater than 1
         n = (n * 3) + 1
-    print([int(n)])
-
+print(int(n), end = " ") 
 
 
