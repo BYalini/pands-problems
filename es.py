e2a960bf8b9025b84b9751c7d19bdd86a74adce0
BYalini <G00387915@gmit.ie>
2020-03-11 21:46:53 +0000

    Weekly task 7

--- es.py
+++ es.py
@@ -1,10 +1,35 @@
-# count the number of e s in moby-dict.txt
-file_name = 'moby-dict.txt'
+# A python script to read in a text file and outputs the number of e's it contains
 
-with open(file_name, 'r') as file : 
-    total =0 
-    for line in file: 
-        total = total+ line.count('e')
+# import interal python module sys to access system argument(s)
+import sys 
+
+# print(sys.argv)
+
+# only runs if one argument is passed with script name (i.e filename to count 'e' s in)
+if len(sys.argv)  == 2 :
+   
+    # argv[0] is script name, argv[1] is file name
+    file_name = sys.argv[1]
+    
+    # define letter to count the occurrence
+    letter = 'e'
+    
+    total = 0
+    
+    #open file with context manager 
+    
+    with open(file_name,'r') as file:
+        # loop through each line in the file
+        for line in file : 
+            # count how manty times 'e' appear in each line
+            total = total + line.count(letter)
+    # context manager closes the file
+        
     print(total)
 
+
+# Ref : https://realpython.com/python-command-line-arguments/
+#import sys
+#print(f"Name of the script      : {sys.argv[0]=}")
+#print(f"Arguments of the script : {sys.argv[1:]=}")
 
