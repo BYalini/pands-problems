2a563a17ae0e735ab4a76d15674900ebad62616c
BYalini <G00387915@gmit.ie>
2020-03-12 22:40:28 +0000

    added references

--- weekday.py
+++ weekday.py
@@ -1,5 +1,7 @@
-#Yalini Brhanavan
 #Program that outputs whether or not today is a weekday
+# Ref: Week 5 lecture : What are dictionaries?  
+# https://web.microsoftstream.com/video/77f26693-82ed-4006-8c22-c61d37e2f77f  (time 08.25)
+
 import datetime
 now = datetime.datetime.now()
 # day_index 0-6 represent the days of the week, starting from Monday.
